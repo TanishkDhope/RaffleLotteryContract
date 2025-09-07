df91cd0aca89bdb1db47e6db58421f75d3b363e3
Tanishk Dhope <tanishkdhope@gmail.com>
2025-09-07 05:32:51 +0000

    ficed error

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -8,7 +8,7 @@ contract DeployRaffle is Script{
     Raffle public raffle;
     function run()external returns(Raffle){
         vm.startBroadcast();
-        raffle=new Raffle(1);
+        raffle=new Raffle(1,1000);
         vm.stopBroadcast();
         return raffle;
     }
